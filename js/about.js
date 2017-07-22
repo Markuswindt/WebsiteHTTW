50b7fd9b681145508d7d7a09b62a5d25bb46c6b9
Markus <Markus-windt@gmx.de>
2017-07-22 21:26:03 +0200

    in richtge bahn gelenkt

--- js/about.js
+++ js/about.js
@@ -1,16 +1,3 @@
-var image, yPos;
-function yScroll(){
-  image = document.getElementById('image');
-  yPos = window.pageYOffset;
-  if(yPos > 35){
-    image.style.height = "20%";
-  } else {
-    image.style.height = "50%";
-  }
-}
-window.addEventListener("scroll", yScroll);
-
-
 var audio = $("#HitMe")[0];
 $(".hitme").mouseenter(function() {
   audio.play()
